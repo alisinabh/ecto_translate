77aef933976546c145ff8827b8a74c630eca37e2
alisinabh <alisina.bm@gmail.com>
2017-11-18 18:39:58 +0330

    Add indents

--- lib/mix_tasks/gen_migration.ex
+++ lib/mix_tasks/gen_migration.ex
@@ -38,24 +38,24 @@ defmodule Mix.Tasks.EctoTranslate.Gen.Migration do
 
   defp change do
     """
-    create table(:test_model) do
-      add :title, :string
-      add :description, :string
-    end
+        create table(:test_model) do
+          add :title, :string
+          add :description, :string
+        end
 
 
-    create table(:translations) do
-      add :translatable_id, #{EctoTranslate.translatable_id_type()}
-      add :translatable_type, :string
-      add :locale, :string
-      add :field, :string
-      add :content, :text
+        create table(:translations) do
+          add :translatable_id, #{EctoTranslate.translatable_id_type()}
+          add :translatable_type, :string
+          add :locale, :string
+          add :field, :string
+          add :content, :text
 
-      timestamps
-    end
-    create index :translations, [:translatable_id, :translatable_type]
-    create index :translations, [:translatable_id, :translatable_type, :locale]
-    create unique_index(:translations, [:translatable_id, :translatable_type, :locale, :field])
+          timestamps
+        end
+        create index :translations, [:translatable_id, :translatable_type]
+        create index :translations, [:translatable_id, :translatable_type, :locale]
+        create unique_index(:translations, [:translatable_id, :translatable_type, :locale, :field])
     """
   end
 
